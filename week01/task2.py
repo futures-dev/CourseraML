b560d807b48d85e75278ae1b0a5cb3bd0f0c32f7
Andrei <futures_dev@outlook.com>
2016-02-01 03:13:54 +0300

    week01 passed

--- week01/task2.py
+++ week01/task2.py
@@ -1,6 +1,16 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
-X = np.array([[1, 2], [3, 4], [5, 6]])
-y = np.array([0, 1, 0])
-clf = DecisionTreeClassifier()
-clf.fit(X, y)
+import pandas
+
+data = pandas.read_csv('titanic.csv', index_col='PassengerId')
+data['Sex'] = data['Sex'].astype('category')
+sex_columns = data.select_dtypes(['category']).columns
+data[sex_columns] = data[sex_columns].apply(lambda x: x.cat.codes)
+data.drop(data.columns[[2, 5, 6, 7, 9, 10]], 1, inplace=True)
+data.dropna(0, inplace=True)
+Y = data['Survived']
+data.drop(data.columns[[0]], 1, inplace=True)
+print(data)
+tree = DecisionTreeClassifier(random_state=241)
+tree.fit(data, Y)
+print(tree.feature_importances_)

4ac39539e5a9631953d9f6fe6d7cc8d72e9f90c1
Andrei@Computer <futures_dev@outlook.com>
2016-02-21 00:06:48 +0300

    week05 passed

--- week04/task1.py
+++ week04/task1.py
@@ -2,6 +2,7 @@ import pandas
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
+from sklearn.linear_model import Ridge
 
 train = pandas.read_csv('salary-train.csv')
 test = pandas.read_csv('salary-test-mini.csv')
@@ -13,11 +14,20 @@ test['FullDescription'] = test['FullDescription'].replace('[^a-zA-Z0-9]', ' ', r
 
 vectorizer = TfidfVectorizer(min_df=5)
 t = vectorizer.fit_transform(train['FullDescription'])
+t1 = vectorizer.transform(test['FullDescription'])
 train['LocationNormalized'].fillna('nan', inplace=True)
 train['ContractTime'].fillna('nan', inplace=True)
+test['LocationNormalized'].fillna('nan', inplace=True)
+test['ContractTime'].fillna('nan', inplace=True)
 
 enc = DictVectorizer()
 X_train_categ = enc.fit_transform(train[['LocationNormalized', 'ContractTime']].to_dict('records'))
 X_test_categ = enc.transform(test[['LocationNormalized', 'ContractTime']].to_dict('records'))
 
-hstack([train])
+new_data = hstack([t, X_train_categ])
+new_test = hstack([t1, X_test_categ])
+
+ridge = Ridge(alpha=1)
+ridge.fit(new_data, train['SalaryNormalized'])
+pr = ridge.predict(new_test)
+print(pr)

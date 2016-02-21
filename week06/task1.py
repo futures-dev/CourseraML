c60501e06ed285d9a6db3fdc3e9c711b1b0dc03a
Andrei@Computer <futures_dev@outlook.com>
2016-02-21 22:00:23 +0300

    week06 done

--- week06/task1.py
+++ week06/task1.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from skimage.io import imread
 from skimage import img_as_float
 from sklearn.cluster import KMeans
@@ -5,9 +6,52 @@ import numpy as np
 
 image = imread('parrots.jpg')
 float_image = img_as_float(image)
+n = 713 * 474
 p = np.array(float_image)
-s = p.reshape(713 * 474, 3)
+s = p.reshape(n, 3)
 pass
-kmeans = KMeans(random_state=241, init='k-means++')
-kmeans.fit(s)
-print(kmeans.n_clusters)
+for N_CLUSTERS in range(1, 20):
+    kmeans = KMeans(random_state=241, init='k-means++', n_clusters=N_CLUSTERS)
+    kmeans.fit(s)
+    new_image = s.copy()
+    print(kmeans.n_clusters)
+    clusters = []
+    for i in range(kmeans.n_clusters):
+        clusters.append([])
+
+    i = 0
+    for pixel in kmeans.labels_:
+        clusters[pixel].append(i)
+        i += 1
+    count = 0
+    for cluster in clusters:
+        avgR = 0
+        avgG = 0
+        avgB = 0
+        for pixel in cluster:
+            avgR += s[pixel][0]
+            avgG += s[pixel][1]
+            avgB += s[pixel][2]
+            count += 1
+        avgR /= len(cluster)
+        avgG /= len(cluster)
+        avgB /= len(cluster)
+        for pixel in cluster:
+            new_image[pixel] = [avgR, avgG, avgB]
+
+    MSE = 0
+    for i in range(n):
+        for color in range(3):
+            MSE += (new_image[i][color] - s[i][color]) ** 2
+
+    MSE /= (3 * n)
+
+    PSNR = 10 * np.log10(1 / MSE)
+    print(PSNR)
+    if PSNR > 20:
+        print("FOUND!! ", N_CLUSTERS)
+    MSE *= 3
+    PSNR = 10 * np.log10(1 / MSE)
+    print(PSNR)
+    if PSNR > 20:
+        print("FOUND!! ", N_CLUSTERS)

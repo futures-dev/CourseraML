827fdfb33c2cbd896a27bd839e319acfc34fca81
Andrei@Computer <futures_dev@outlook.com>
2016-02-23 23:26:35 +0300

    week05 in progress

--- week05/task2.py
+++ week05/task2.py
@@ -5,22 +5,34 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import log_loss
 
 data = pandas.read_csv('gbm-data.csv')
-train = data.drop(0).values
-y = data[1:].values
+y = data['Activity'].values
+train = data.drop('Activity', axis=1).values
 X_train, X_test, y_train, y_test = train_test_split(train, y, test_size=0.8, random_state=241)
-classifier = GradientBoostingClassifier(n_estimators=250, verbose=True, random_state=241)
 result = {}
+"""
 for learning_rate in [1, .5, .3, .2, .1]:
+"""
+for learning_rate in [.2]:
+    classifier = GradientBoostingClassifier(n_estimators=250, verbose=True, random_state=241,
+                                            learning_rate=learning_rate)
     classifier.fit(X_train, y_train)
     sdf_train = classifier.staged_decision_function(X_train)
     sdf_test = classifier.staged_decision_function(X_test)
-    hell = 1 / (1 + np.e ** (-classifier.predict(X_test)))
-    log_loss_train = log_loss(y_train, classifier.predict(X_train))
-    log_loss_test = log_loss(y_test, classifier.predict(X_test))
-    result[learning_rate] = {
-        'sdf_train': sdf_train,
-        'sdf_test': sdf_test,
-        'hell': hell,
-        'log_loss_train': log_loss_train,
-        'log_loss_test': log_loss_test}
+    y_train_pred = []
+    y_test_pred = []
+    log_train = []
+    log_test = []
+    for i, pred in enumerate(sdf_train):
+        y_train_pred.append(1 / (1 + np.exp(-pred)))
+        log_train.append(log_loss(y_train, y_train_pred[i]))
+    m = 19999
+    ii = 0
+    for i, pred in enumerate(sdf_test):
+        y_test_pred.append(1 / (1 + np.exp(-pred)))
+        log_test.append(log_loss(y_test, y_test_pred[i]))
+        if log_test[i] < m:
+            m = log_test[i]
+            ii = i
+
+    print(m, ii)
 pass
